375ada18a41935809693b510561160a6871ddb82
Alondra-Plascencia <alondra.plascencia9334@alumnos.udg.mx>
2025-07-09 19:13:25 -0500

    Se agregaron las funciones para remover caracteres no alphanumericos y la función para convertir mayus a mins para los titulares y comentarios.

--- modules/run_polarity.py
+++ modules/run_polarity.py
@@ -25,6 +25,7 @@ import torch
 
 #External Modules
 from module_data_path import df_data_path, plot_data_path
+from module_cleansing import remover_no_alfanumericos
 
 stages = [1]
 
@@ -33,10 +34,21 @@ def stage1():
     # data files directory path
     data_path = df_data_path()
 
-    titulares_df = os.path.join(data_path, 'titulares_prueba.csv')
-    coments_df = os.path.join(data_path, 'comentarios_prueba.csv')
-    print('Stage 1')
-
+    titulares = os.path.join(data_path, 'titulares_prueba.csv')
+    comentarios = os.path.join(data_path, 'comentarios_prueba.csv')
+    
+    titulares_df = pd.read_csv(titulares, encoding='ISO-8859-1')
+    comentarios_df = pd.read_csv(comentarios, encoding='ISO-8859-1')
+    
+    titulares_df['Tweet'] = titulares_df['Tweet'].apply(remover_no_alfanumericos).str.lstrip().str.lower()
+    
+    # Reemplaza 'titulares_df' con el DataFrame que estás utilizando
+    for index, row in comentarios_df.iterrows():  # Recorremos cada fila
+        for col in comentarios_df.columns[3:]:  # Recorremos las columnas a partir de la 3ra columna (índice 2)
+            if pd.notna(row[col]):  # Verifica que el valor no sea NaN
+                # Aplica las transformaciones a los valores que no son NaN
+                comentarios_df.at[index, col] = remover_no_alfanumericos(row[col]).lstrip().lower()
+        
 
 
 
